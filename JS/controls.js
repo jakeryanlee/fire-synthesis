ee35d85f0b72f34a626b195628986b1c2dee0e06
JakeLee <jakeryanlee@gmail.com>
2017-10-11 16:10:46 +0100

    NexusUI added along with some controls and visualiser.

--- JS/controls.js
+++ JS/controls.js
@@ -1,17 +1,39 @@
-/**
- * Event handler for the play button
- * @type {Element}
- */
-var playButton = document.getElementById("play");
-playButton.onclick = function () {
-    param.node.parameters.setParameterByName('gain', 70);
-};
+var muteSwitch = new Nexus.Toggle('#mute', {
+    'size' : [40, 20],
+    'state' : false
+});
 
-/**
- * Event handler for the stop button
- * @type {Element}
- */
-var stopButton = document.getElementById("stop");
-stopButton.onclick = function() {
-    param.node.parameters.setParameterByName('gain', 0);
-};
+muteSwitch.on('change', function(v) {
+    if(v) {
+        param.node.parameters.setParameterByName('gain', 12);
+    } else {
+        param.node.parameters.setParameterByName('gain', 0);
+    }
+});
+
+var lappingSlider = new Nexus.Slider('#lappingIntensity', {
+    'size': [120,20],
+    'mode': 'relative',  // 'relative' or 'absolute'
+    'min': 0,
+    'max': 1,
+    'step': 0,
+    'value': 0
+});
+
+var cracklingSlider = new Nexus.Slider('#cracklingIntensity', {
+    'size': [120,20],
+    'mode': 'relative',  // 'relative' or 'absolute'
+    'min': 0,
+    'max': 1,
+    'step': 0,
+    'value': 0
+});
+
+var hissingSlider = new Nexus.Slider('#hissingIntensity', {
+    'size': [120,20],
+    'mode': 'relative',  // 'relative' or 'absolute'
+    'min': 0,
+    'max': 1,
+    'step': 0,
+    'value': 0
+});
--- JS/main.js
+++ JS/main.js
@@ -1,6 +1,10 @@
 window.AudioContext = (window.AudioContext || window.webkitAudioContext || window.mozAudioContext);
 
 var context = new AudioContext();
+Nexus.context = context;
+var spectrogram = new Nexus.Spectrogram('#visualiser',{
+    'size': [300,150]
+});
 var start = context.createGain();
 var end = context.createGain();
 var Factory = new PluginFactory(context);
@@ -8,5 +12,6 @@ Factory.addPrototype(FirePlugin);
 
 var subFactory = Factory.createSubFactory(start, end);
 var param = subFactory.createPlugin(Factory.getPrototypes()[0]);
-
+param.node.parameters.setParameterByName('gain', 0);
+spectrogram.connect(end);
 end.connect(context.destination);
